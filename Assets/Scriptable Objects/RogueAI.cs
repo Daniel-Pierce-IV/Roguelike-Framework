b24e86d220d401f8dd05a3262b530c6b6807a433
Daniel Pierce <daniel.pierce.iv@gmail.com>
2020-01-27 23:51:23 -0500

    Implement simple ai.

--- Assets/Scriptable Objects/RogueAI.cs
+++ Assets/Scriptable Objects/RogueAI.cs
@@ -3,8 +3,37 @@
 [CreateAssetMenu(fileName = "New AI", menuName = "Artificial Intelligence")]
 public class RogueAI : ScriptableObject
 {
-	public void Act(Entity entity, RogueMap roguemap)
+	public void Act(Entity entity, RogueMap rogueMap)
 	{
-		Debug.Log("The " + entity.data.name + " is itching for a fight.");
+		// Attack player on sight
+		const float sightRange = 5f;
+		const float attackRange = 2f;
+
+		Vector2Int direction = rogueMap.Player.Position - entity.Position;
+
+		if (direction.magnitude < attackRange)
+		{
+			rogueMap.AttackEntity(entity, rogueMap.Player);
+		}
+		else if (direction.magnitude < sightRange)
+		{
+			Vector2Int newPosition = entity.Position + NormalizedVector2Int(direction);
+			
+			if (rogueMap.PositionCanBeMovedTo(newPosition))
+			{
+				rogueMap.MoveEntityToPosition(entity, newPosition);
+			}
+		}
+		//Debug.Log("The " + entity.data.name + " is itching for a fight.");
+	}
+
+	// "Normalize" the direction to integer values (best approximation)
+	// NOTE: "normalized" interger direction always adds up to 0, 1, or 2
+	Vector2Int NormalizedVector2Int(Vector2Int direction)
+	{
+		Vector2 floatDirection = new Vector2(direction.x, direction.y);
+		float distance = floatDirection.magnitude;
+
+		return Vector2Int.RoundToInt(floatDirection / distance);
 	}
 }
